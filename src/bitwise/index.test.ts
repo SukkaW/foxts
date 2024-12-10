84da4e5f8ed643fea4c8172d1c5fce3fd30921f8
SukkaW <isukkaw@gmail.com>
2024-12-11 00:59:57 +0800

    feat: add `bitwise`

--- src/bitwise/index.test.ts
+++ src/bitwise/index.test.ts
@@ -1,21 +1,53 @@
-import { pack, unpack, unpackFirst, unpackSecond } from './index';
+import { bitCount, getBit, setBit, deleteBit, packTwoBits, unpackTwoBits, unpackTwoBitsFirst, unpackTwoBitsSecond, packThreeBits, unpackThreeBits } from './index';
 import expect from 'expect';
 
 describe('bitwise', () => {
-  it('pack', () => {
-    expect(pack(0x1234, 0x5678)).toBe(0x12_34_56_78);
+  it('bitCount', () => {
+    expect(bitCount(0b1010)).toBe(2);
+    expect(bitCount(0b1011)).toBe(3);
+    expect(bitCount(0b1000)).toBe(1);
   });
 
-  it('unpack', () => {
-    const arr = pack(0x1234, 0x5678);
-    expect(unpack(arr)).toStrictEqual([0x1234, 0x5678]);
+  it('getBit', () => {
+    expect(getBit(0b1010, 0b1000)).toBe(true);
+    expect(getBit(0b1010, 0b0100)).toBe(false);
   });
 
-  it('unpackFirst', () => {
-    expect(unpackFirst(0x12_34_56_78)).toBe(0x1234);
+  it('setBit', () => {
+    expect(setBit(0b1010, 0b0100)).toBe(0b1110);
+    expect(setBit(0b1010, 0b1000)).toBe(0b1010);
   });
 
-  it('unpackSecond', () => {
-    expect(unpackSecond(0x12_34_56_78)).toBe(0x5678);
+  it('clearBit', () => {
+    expect(deleteBit(0b1010, 0b1000)).toBe(0b0010);
+    expect(deleteBit(0b1010, 0b0100)).toBe(0b1010);
+  });
+
+  it('packTwoBits', () => {
+    expect(packTwoBits(0x1234, 0x5678)).toBe(0x12_34_56_78);
+  });
+
+  it('unpackTwoBits', () => {
+    const arr = packTwoBits(0x1234, 0x5678);
+    expect(unpackTwoBits(arr)).toStrictEqual([0x1234, 0x5678]);
+  });
+
+  it('unpackTwoBitsFirst', () => {
+    expect(unpackTwoBitsFirst(0x12_34_56_78)).toBe(0x1234);
+  });
+
+  it('unpackTwoBitsSecond', () => {
+    expect(unpackTwoBitsSecond(0x12_34_56_78)).toBe(0x5678);
+  });
+
+  it('packThreeBits', () => {
+    console.log(0x3FF, packThreeBits(0x3FF, 0x3FF, 0x3FF).toString(16));
+    expect(packThreeBits(0x123, 0x456, 0x789)).toBe(0x0_12_31_5F_89);
+    expect(packThreeBits(0x3FF, 0x3FF, 0x3FF)).toBe(0x3F_FF_FF_FF);
+  });
+
+  it('unpackThreeBits', () => {
+    const arr = packThreeBits(0x114, 0x51, 0x41);
+    expect(unpackThreeBits(arr)).toStrictEqual([0x114, 0x51, 0x41]);
   });
 });
--- src/bitwise/index.ts
+++ src/bitwise/index.ts
@@ -1,17 +1,54 @@
-/** Packs two 16-bit integers into one 32-bit integer */
-export const pack = (a: number, b: number): number => (a << 16) | b;
+// From: https://stackoverflow.com/a/43122214/1185079
+export function bitCount(n: number): number {
+  n = n - ((n >> 1) & 0x55_55_55_55);
+  n = (n & 0x33_33_33_33) + ((n >> 2) & 0x33_33_33_33);
+  return (((n + (n >> 4)) & 0xF_0F_0F_0F) * 0x1_01_01_01) >> 24;
+}
 
+export function getBit(n: number, mask: number): boolean {
+  return !!(n & mask);
+}
+
+export function setBit(n: number, mask: number): number {
+  return n | mask;
+}
+
+export function deleteBit(n: number, mask: number): number {
+  return n & ~mask;
+}
+
+/** Packs two 16-bit integers (0~65535) into one 32-bit integer */
+export const packTwoBits = (a: number, b: number): number => (a << 16) | b;
+
+export const unpackTwoBitsFirst = (value: number): number => (value >> 16) & 0xFFFF;
+export const unpackTwoBitsSecond = (value: number): number => value & 0xFFFF;
 /**
- * Unpacks two 16-bit integers from one 32-bit integer
+ * Unpacks two 16-bit integers (0~65535) from one 32-bit integer
  *
  * @param value - The 32-bit integer to unpack
  * @param arr - An optional array to store the unpacked values, useful if you are trying to re-use arrays
  */
-export function unpack(value: number, arr: [a: number, b: number] = Array.from(new Array(2).keys()) as any): [a: number, b: number] {
-  arr[0] = (value >> 16) & 0xFFFF;
-  arr[1] = value & 0xFFFF;
+export function unpackTwoBits(value: number, arr: [a: number, b: number] = Array.from(new Array(2).keys()) as any): [a: number, b: number] {
+  arr[0] = unpackTwoBitsFirst(value);
+  arr[1] = unpackTwoBitsSecond(value);
   return arr;
 }
 
-export const unpackFirst = (value: number): number => (value >> 16) & 0xFFFF;
-export const unpackSecond = (value: number): number => value & 0xFFFF;
+/** Packs three 10-bit integers (0~1023) into one 32-bit integer */
+export const packThreeBits = (a: number, b: number, c: number): number => (a << 20) | (b << 10) | c;
+
+export const unpackThreeBitsFirst = (value: number): number => (value >> 20) & 0x3FF;
+export const unpackThreeBitsSecond = (value: number): number => (value >> 10) & 0x3FF;
+export const unpackThreeBitsThird = (value: number): number => value & 0x3FF;
+/**
+ * Unpacks three 10-bit integers (0~1023) from one 32-bit integer
+ *
+ * @param value - The 32-bit integer to unpack
+ * @param arr - An optional array to store the unpacked values, useful if you are trying to re-use arrays
+ */
+export function unpackThreeBits(value: number, arr: [a: number, b: number, c: number] = Array.from(new Array(3).keys()) as any): [a: number, b: number, c: number] {
+  arr[0] = unpackThreeBitsFirst(value);
+  arr[1] = unpackThreeBitsSecond(value);
+  arr[2] = unpackThreeBitsThird(value);
+  return arr;
+}
