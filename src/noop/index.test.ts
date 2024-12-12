e6d286f9864c8894bcaa37e928a9a385f9cbb4ff
SukkaW <isukkaw@gmail.com>
2024-12-12 19:57:10 +0800

    feat: add more noop

--- src/noop/index.test.ts
+++ src/noop/index.test.ts
@@ -1,9 +1,21 @@
 import { describe, it } from 'mocha';
 import { expect } from 'expect';
-import { noop } from '.';
+import { falseFn, noop, throwFn, trueFn } from '.';
 
 describe('noop', () => {
   it('noop', () => {
     expect(noop()).toBe(undefined);
+  });
+
+  it('trueFn', () => {
+    expect(trueFn()).toBe(true);
+  });
+
+  it('falseFn', () => {
+    expect(falseFn()).toBe(false);
+  });
+
+  it('throwFn', () => {
+    expect(() => throwFn()).toThrow();
   });
 });
--- src/noop/index.ts
+++ src/noop/index.ts
@@ -1,4 +1,10 @@
-interface Noop {
-  (...args: any[]): any
+interface Noop<T = any> {
+  (...args: any[]): T
 }
 export const noop: Noop = () => { /* noop */ };
+
+export const trueFn: Noop<true> = () => true;
+export const falseFn: Noop<false> = () => false;
+
+// eslint-disable-next-line sukka/unicorn/error-message -- deliberately w/o error msg
+export const throwFn: Noop<never> = () => { throw new Error(); };
