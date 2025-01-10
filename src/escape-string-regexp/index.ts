06f1dacb28a6d373da654ae333bd88840126f260
SukkaW <isukkaw@gmail.com>
2025-01-10 09:25:34 +0800

    perf(escape-string-regexp): run once

--- src/escape-string-regexp/index.ts
+++ src/escape-string-regexp/index.ts
@@ -1,11 +1,29 @@
-const r = /[$()*+.?[\\\]^{|}]/g;
+const r = /[$()*+.?[\\\]^{|}-]/g;
+
+const escapes = {
+  /* eslint-disable sukka/unicorn/prefer-string-raw -- performance */
+  '\\': '\\\\',
+  '^': '\\^',
+  $: '\\$',
+  '.': '\\.',
+  '*': '\\*',
+  '+': '\\+',
+  '?': '\\?',
+  '(': '\\(',
+  ')': '\\)',
+  '[': '\\[',
+  ']': '\\]',
+  '{': '\\{',
+  '}': '\\}',
+  '|': '\\|',
+  '-': '\\x2d'
+  /* eslint-enable sukka/unicorn/prefer-string-raw */
+};
+
+const replacer = (chr: string) => escapes[chr as keyof typeof escapes];
 
 export function escapeStringRegexp(string: string) {
   // Escape characters with special meaning either inside or outside character sets.
   // Use a simple backslash escape when it’s always valid, and a `\xnn` escape when the simpler form would be disallowed by Unicode patterns’ stricter grammar.
-  return string
-    // eslint-disable-next-line sukka/unicorn/prefer-string-raw -- performance
-    .replaceAll(r, '\\$&')
-    // eslint-disable-next-line sukka/unicorn/prefer-string-raw -- performance
-    .replaceAll('-', '\\x2d');
+  return string.replaceAll(r, replacer);
 };
