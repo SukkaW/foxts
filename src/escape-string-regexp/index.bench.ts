f87046212bee903b7a965436f80539ebc9f4427a
SukkaW <isukkaw@gmail.com>
2025-03-10 23:11:52 +0800

    perf(escape-string-regexp): improve performance by 400%

---
+++ src/escape-string-regexp/index.bench.ts
@@ -0,0 +1,37 @@
+import { escapeStringRegexp } from '.';
+import { escapeRegExp as hexoUtilEscapeRegExp } from 'hexo-util';
+import escapeStringRegexpSindresorhus from 'escape-string-regexp';
+import escapeRegExp from 'escape-regexp';
+import lodashEscapeRegExp from 'lodash.escaperegexp';
+import RegexEscape from 'regex-escape';
+
+(async () => {
+  const { bench, group, run } = await import('mitata');
+
+  const fns = [
+    ['foxts/escape-string-regexp', escapeStringRegexp],
+    ['hexo-util', hexoUtilEscapeRegExp],
+    ['escape-string-regexp', escapeStringRegexpSindresorhus],
+    ['escape-regexp', escapeRegExp],
+    ['lodash.escaperegexp', lodashEscapeRegExp],
+    ['regex-escape', RegexEscape]
+  ] as const;
+
+  group('short', () => {
+    const fixture = '{#/}';
+
+    fns.forEach(([name, fn]) => {
+      bench(name, () => fn(fixture));
+    });
+  });
+
+  group('full', () => {
+    const fixture = String.raw`^$\.*+?()[]{}|`;
+
+    fns.forEach(([name, fn]) => {
+      bench(name, () => fn(fixture));
+    });
+  });
+
+  run();
+})();
--- src/escape-string-regexp/index.ts
+++ src/escape-string-regexp/index.ts
@@ -1,29 +1,85 @@
-const r = /[$()*+.?[\\\]^{|}-]/g;
+const r = /[$()*+.?[\\\]^{|}-]/;
 
-const escapes = {
-  /* eslint-disable sukka/unicorn/prefer-string-raw -- performance */
-  '\\': '\\\\',
-  '^': '\\^',
-  $: '\\$',
-  '.': '\\.',
-  '*': '\\*',
-  '+': '\\+',
-  '?': '\\?',
-  '(': '\\(',
-  ')': '\\)',
-  '[': '\\[',
-  ']': '\\]',
-  '{': '\\{',
-  '}': '\\}',
-  '|': '\\|',
-  '-': '\\x2d'
-  /* eslint-enable sukka/unicorn/prefer-string-raw */
-};
+/**
+ * Escape characters with special meaning either inside or outside character sets.
+ * Use a simple backslash escape when it’s always valid, and a `\xnn` escape when the simpler form would be disallowed by Unicode patterns’ stricter grammar.
+ */
+export function escapeStringRegexp(str: string) {
+  const match = r.exec(str);
 
-const replacer = (chr: string) => escapes[chr as keyof typeof escapes];
+  if (match === null) {
+    return str;
+  }
 
-export function escapeStringRegexp(string: string) {
-  // Escape characters with special meaning either inside or outside character sets.
-  // Use a simple backslash escape when it’s always valid, and a `\xnn` escape when the simpler form would be disallowed by Unicode patterns’ stricter grammar.
-  return string.replaceAll(r, replacer);
+  let escape = '';
+  let regexp = '';
+
+  let index = match.index;
+  let lastIndex = 0;
+
+  // iterate from the first match
+  for (const len = str.length; index < len; index++) {
+    /* eslint-disable sukka/unicorn/prefer-string-raw -- performance */
+    switch (str.charCodeAt(index)) {
+      case 92: // \
+        escape = '\\\\';
+        break;
+      case 94: // ^
+        escape = '\\^';
+        break;
+      case 36: // $
+        escape = '\\$';
+        break;
+      case 46: // .
+        escape = '\\.';
+        break;
+      case 42: // *
+        escape = '\\*';
+        break;
+      case 43: // +
+        escape = '\\+';
+        break;
+      case 63: // ?
+        escape = '\\?';
+        break;
+      case 40: // (
+        escape = '\\(';
+        break;
+      case 41: // )
+        escape = '\\)';
+        break;
+      case 91: // [
+        escape = '\\[';
+        break;
+      case 93: // ]
+        escape = '\\]';
+        break;
+      case 123: // {
+        escape = '\\{';
+        break;
+      case 125: // }
+        escape = '\\}';
+        break;
+      case 124: // |
+        escape = '\\|';
+        break;
+      case 45: // -
+        escape = '\\x2d';
+        break;
+      default:
+        continue;
+    }
+    /* eslint-enable sukka/unicorn/prefer-string-raw */
+
+    if (lastIndex !== index) {
+      regexp += str.slice(lastIndex, index);
+    }
+
+    lastIndex = index + 1;
+    regexp += escape;
+  }
+
+  return lastIndex === index
+    ? regexp
+    : regexp + str.slice(lastIndex, index);
 };
