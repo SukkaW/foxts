ab7a3b8decc1bdec12dbf77d4873c3dc3b9ca10a
SukkaW <isukkaw@gmail.com>
2025-06-02 00:37:27 +0800

    feat(once): supports `prewarm` options

--- src/once/index.ts
+++ src/once/index.ts
@@ -1,6 +1,11 @@
-export function once<T>(fn: (() => T)): (() => T) {
+export function once<T>(fn: (() => T), prewarm = true): (() => T) {
   let called = false;
   let result: T;
+
+  if (prewarm) {
+    result = fn();
+    return () => result;
+  }
 
   return (): T => {
     if (!called) {
