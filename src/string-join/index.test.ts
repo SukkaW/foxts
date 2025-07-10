d8cbb7eebf78b34120ab17c903f60ba313583633
SukkaW <isukkaw@gmail.com>
2025-07-10 21:02:16 +0800

    feat(string-join): support all falsy value

--- src/string-join/index.test.ts
+++ src/string-join/index.test.ts
@@ -4,12 +4,15 @@ import { stringJoin } from '.';
 
 describe('string-join', () => {
   it('should work', () => {
+    expect(stringJoin([])).toBe('');
     expect(stringJoin([null, '1'], ' ')).toBe('1');
     expect(stringJoin([undefined, '1', '2'], ' ')).toBe('1 2');
     expect(stringJoin(['1', '2'])).toBe('1,2');
   });
 
   it('dedupe', () => {
+    expect(stringJoin([], ' ', true)).toBe('');
+    expect(stringJoin(new Array(2), ' ', true)).toBe('');
     expect(stringJoin(['1', '1', '2'], ' ', true)).toBe('1 2');
   });
 });
--- src/string-join/index.ts
+++ src/string-join/index.ts
@@ -1,9 +1,29 @@
-import { fastStringArrayJoin } from '../fast-string-array-join';
-import { isNonNullish } from '../guard';
+export function stringJoin(arr: Array<string | null | undefined | false | 0>, sep = ',', dedupe = false): string {
+  if (arr.length === 0) {
+    return '';
+  }
 
-export function stringJoin(arr: Array<string | null | undefined>, sep = ',', dedupe = false): string {
   if (dedupe) {
     arr = Array.from(new Set(arr));
   }
-  return fastStringArrayJoin(arr.filter(isNonNullish), sep);
+
+  const len = arr.length;
+
+  let result = '';
+  let item;
+
+  for (let i = 0; i < len - 1; i++) {
+    item = arr[i];
+    if (item) {
+      result += item;
+      result += sep;
+    }
+  }
+
+  item = arr[len - 1];
+  if (item) {
+    result += item;
+  }
+
+  return result;
 }
