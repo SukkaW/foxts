aa81b8192ad83ab659503622c497c1e32c59f631
SukkaW <isukkaw@gmail.com>
2025-09-28 00:51:01 +0800

    refactor(empty-node): change exported methods

--- src/empty-node/index.ts
+++ src/empty-node/index.ts
@@ -1,17 +1,20 @@
-export function emptyNodeLegacy(node: Node) {
+/**
+ * if you are trying to empty an Element, use `emptyElement` method instead, it will opt-in more performant method on modern browsers.
+ */
+export function emptyNode(node: Node) {
   while (node.firstChild) { // get first child is always faster
     // remove last child is way faster (pop always faster than shift)
     node.removeChild(node.lastChild!);
   }
 }
 
-export function emptyNodeModern(node: Element) {
-  node.replaceChildren();
+export function emptyElementModern(element: Element) {
+  element.replaceChildren();
 }
 
 /**
  * Use `Element.replaceChildren` if available, otherwise fall back to removing each child node.
  */
-export const emptyNode = typeof Element !== 'undefined' && 'replaceChildren' in Element.prototype
-  ? emptyNodeModern
-  : emptyNodeLegacy;
+export const emptyElement: (element: Element) => void = typeof Element !== 'undefined' && 'replaceChildren' in Element.prototype
+  ? emptyElementModern
+  : emptyNode;
