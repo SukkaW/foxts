c13b175f90e480875168fe7376194d81a06b2cd7
SukkaW <isukkaw@gmail.com>
2025-09-28 00:40:13 +0800

    feat(empty-node): bring up

---
+++ src/empty-node/index.ts
@@ -0,0 +1,17 @@
+export function emptyNodeLegacy(node: Node) {
+  while (node.firstChild) { // get first child is always faster
+    // remove last child is way faster (pop always faster than shift)
+    node.removeChild(node.lastChild as ChildNode);
+  }
+}
+
+export function emptyNodeModern(node: Element) {
+  node.replaceChildren();
+}
+
+/**
+ * Use `Element.replaceChildren` if available, otherwise fall back to removing each child node.
+ */
+export const emptyNode = typeof Element !== 'undefined' && 'replaceChildren' in Element.prototype
+  ? emptyNodeModern
+  : emptyNodeLegacy;
