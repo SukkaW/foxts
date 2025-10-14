0cc56062958bbe6670d18a58902bfa7a3d4e094f
SukkaW <isukkaw@gmail.com>
2025-10-15 00:23:19 +0800

    perf(guard): hoist fn

--- src/guard/index.ts
+++ src/guard/index.ts
@@ -1,3 +1,9 @@
+const $not_null = <T>(i: T | null): i is T => i !== null;
+const $not_undefined = <T>(i: T | undefined): i is T => i !== undefined;
+const $not_false = <T>(i: T | false): i is T => i !== false;
+const $not_nullish = <T>(i: T | null | undefined): i is T => i != null;
+const $not_falsy = <T>(i: T | 0 | '' | false | null | undefined): i is T => !!i;
+
 export function not(arg: null): <T>(i: T | null) => i is T;
 export function not(arg: undefined): <T>(i: T | undefined) => i is T;
 export function not(arg: false): <T>(i: T | false) => i is T;
@@ -6,19 +12,26 @@ export function not(arg: 'falsy'): <T>(i: T | 0 | '' | false | null | undefined)
 export function not(arg: null | undefined | false | 'nullish' | 'falsy') {
   switch (arg) {
     case null:
-      return <T>(i: T | null): i is T => i !== null;
+      return $not_null;
     case undefined:
-      return <T>(i: T | undefined): i is T => i !== undefined;
+      return $not_undefined;
     case false:
-      return <T>(i: T | false): i is T => i !== false;
+      return $not_false;
     case 'nullish':
-      return <T>(i: T | null | undefined): i is T => i != null;
+      return $not_nullish;
     case 'falsy':
-      return <T>(i: T | 0 | '' | false | null | undefined): i is T => !!i;
+      return $not_falsy;
     default:
       never(arg, 'argument');
   }
 }
+
+const $is_null = (i: unknown): i is null => i === null;
+const $is_undefined = (i: unknown): i is undefined => i === undefined;
+const $is_false = (i: unknown): i is false => i === false;
+const $is_nullish = (i: unknown): i is null | undefined => i == null;
+const $is_falsy = (i: unknown): i is 0 | '' | false | null | undefined => !i;
+const $is_truthy = <T>(i: T | 0 | '' | false | null | undefined): i is T => !!i;
 
 export function is(arg: null): (i: unknown) => i is null;
 export function is(arg: undefined): (i: unknown) => i is undefined;
@@ -29,26 +42,26 @@ export function is(arg: 'truthy'): <T>(i: T | 0 | '' | false | null | undefined)
 export function is(arg: null | undefined | false | 'nullish' | 'falsy' | 'truthy') {
   switch (arg) {
     case null:
-      return (i: unknown): i is null => i === null;
+      return $is_null;
     case undefined:
-      return (i: unknown): i is undefined => i === undefined;
+      return $is_undefined;
     case false:
-      return (i: unknown): i is false => i === false;
+      return $is_false;
     case 'nullish':
-      return (i: unknown): i is null | undefined => i == null;
+      return $is_nullish;
     case 'falsy':
-      return (i: unknown): i is 0 | '' | false | null | undefined => !i;
+      return $is_falsy;
     case 'truthy':
-      return <T>(i: T | 0 | '' | false | null | undefined): i is T => !!i;
+      return $is_truthy;
     default:
       never(arg, 'argument');
   }
 }
 
-export const isTruthy = is('truthy');
-export const isFalsy = is('falsy');
-export const isNonNull = not(null);
-export const isNonNullish = not('nullish');
+export const isTruthy = $is_truthy;
+export const isFalsy = $is_falsy;
+export const isNonNull = $not_null;
+export const isNonNullish = $not_nullish;
 
 export function nullthrow<T>(value: T | null | undefined, message = '[foxts/invariant] "value" is null or undefined'): T {
   if (value === null || value === undefined) {
