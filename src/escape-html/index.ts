ce7b61938d6fa278fd66c6a5ff3dee8155c013e8
SukkaW <isukkaw@gmail.com>
2025-10-19 17:08:28 +0800

    refactor: deprecate all re-exports

--- src/escape-html/index.ts
+++ src/escape-html/index.ts
@@ -1,1 +1,7 @@
-export * from 'fast-escape-html';
+/* eslint-disable sukka/unicorn/prefer-export-from -- deprecate re-export */
+import { escapeHTML as A, unescapeHTML as B } from 'fast-escape-html';
+
+/** @deprecated This re-export is deprecated. Install and import from 'fast-escape-html' directly instead. This will be removed in the next major version. */
+export const escapeHTML = A;
+/** @deprecated This re-export is deprecated. Install and import from 'fast-escape-html' directly instead. This will be removed in the next major version. */
+export const unescapeHTML = B;
--- src/escape-string-regexp/index.ts
+++ src/escape-string-regexp/index.ts
@@ -1,1 +1,4 @@
-export { escapeRegexp as escapeStringRegexp } from 'fast-escape-regexp';
+import { escapeRegexp } from 'fast-escape-regexp';
+/** @deprecated This re-export is deprecated. Install and import from 'fast-escape-regexp' directly instead. This will be removed in the next major version. */
+// eslint-disable-next-line sukka/unicorn/prefer-export-from -- deprecated re-export
+export const escapeStringRegexp = escapeRegexp;
