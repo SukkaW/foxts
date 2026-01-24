b92bff8475a6a3ef2c030e73b87ba76916877687
SukkaW <isukkaw@gmail.com>
2026-01-24 22:37:43 +0800

    feat(base92): bring up

---
+++ src/base92/index.test.ts
@@ -0,0 +1,13 @@
+import { expect } from 'expect';
+import { base92ToUint8Array, uint8ArrayToBase92 } from '.';
+import { stringToUint8Array } from '../uint8array-utils';
+
+describe('base92', () => {
+  it('should convert uint8ArrayToBase92 and base92ToUint8Array', () => {
+    const fixture = stringToUint8Array('a Ā 𐀀 文 🦄');
+
+    const base92 = uint8ArrayToBase92(fixture);
+    expect(base92).toBe('Rg#ZGO:G7\'PdO6mMs;6^mN');
+    expect(base92ToUint8Array(base92)).toEqual(fixture);
+  });
+});
