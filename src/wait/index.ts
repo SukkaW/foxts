7567145a4ac89ec0a29a07e3a36f924cdff15ee6
SukkaW <isukkaw@gmail.com>
2026-03-27 18:38:28 +0800

    feat(wait): add `unref` option

--- src/wait/index.ts
+++ src/wait/index.ts
@@ -1,11 +1,17 @@
-export function wait(ms: number): Promise<void> {
+export function wait(ms: number, timerUnref = false): Promise<void> {
   return new Promise<void>(resolve => {
-    // eslint-disable-next-line sukka/prefer-timer-id -- hang timers
-    setTimeout(resolve, ms);
+    const timer = setTimeout(resolve, ms);
+    if (timerUnref && typeof timer === 'object' && 'unref' in timer && typeof timer.unref === 'function') {
+      timer.unref();
+    }
   });
 }
 
-export function waitWithAbort(ms: number, signal: AbortSignal): Promise<void> {
+export function waitWithAbort(ms: number, signal: AbortSignal | undefined | null, timerUnref = false): Promise<void> {
+  if (!signal) {
+    return wait(ms, timerUnref);
+  }
+
   return new Promise<void>((resolve, reject) => {
     if (signal.aborted) {
       reject(signal.reason as Error);
@@ -15,14 +21,18 @@ export function waitWithAbort(ms: number, signal: AbortSignal): Promise<void> {
     const timer = setTimeout(() => {
       signal.removeEventListener('abort', onAbort);
       resolve();
+
+      if (timerUnref && typeof timer === 'object' && 'unref' in timer && typeof timer.unref === 'function') {
+        timer.unref();
+      }
     }, ms);
 
     function onAbort() {
       clearTimeout(timer);
-      signal.removeEventListener('abort', onAbort);
-      reject(signal.reason as Error);
+      signal?.removeEventListener('abort', onAbort);
+      reject(signal?.reason as Error);
     };
 
-    signal.addEventListener('abort', onAbort);
+    signal.addEventListener('abort', onAbort, { once: true });
   });
 }
